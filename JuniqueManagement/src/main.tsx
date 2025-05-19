783e1da979a0e2020e8456cf1948dc562e9e0e7c
longtn <long.tn3101@gmail.com>
2025-05-19 20:16:46 +0700

    a

--- JuniqueManagement/src/main.tsx
+++ JuniqueManagement/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import theme from './layout/theme.ts'
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material'
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
     <ThemeProvider theme={theme}>
